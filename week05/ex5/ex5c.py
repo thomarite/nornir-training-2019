d44c254d36ca52d3ca273fafbcf8dbf54ffa24cd
tmorales <tomas.morales@gmail.com>
2019-10-13 16:12:18 -0700

    finished week5\!

--- week05/ex5/ex5c.py
+++ week05/ex5/ex5c.py
@@ -4,6 +4,28 @@ from nornir.plugins.tasks import text
 from nornir.plugins.tasks import files
 from nornir.plugins.tasks import data
 from nornir.core.filter import F
+from nornir.plugins.tasks import networking
+import time
+
+def verify_bgp(task):
+  results = task.run(task=networking.napalm_get,getters=["bgp_neighbors"])
+  if results[0].result['bgp_neighbors']['global']['peers'][task.host["bgp_peer"]]['is_up']: 
+     print("BGP configured and UP")
+  else:
+     print("BGP is no UP!")
+
+  #import ipdb; ipdb.set_trace()
+  print(results[0].result)
+
+def deploy_configs(task):
+  filename = f"rendered/{task.host.name}_interfaces"
+  with open(filename, "r") as f:
+     cfg = f.read()
+  result = task.run(task=networking.napalm_configure, configuration=cfg)
+  filename = f"rendered/{task.host.name}_bgp"
+  with open(filename, "r") as f:
+     cfg = f.read()
+  result = task.run(task=networking.napalm_configure, configuration=cfg)
 
 def write_configs(task):
   config_path = f"./rendered/"
@@ -35,6 +57,12 @@ def main():
   nr.run(task=nxos_bgp)
   write = nr.run(task=write_configs)
   print_result(write)
+  deploy = nr.run(task=deploy_configs)
+  print_result(deploy)
+  time.sleep(10)
+  verify = nr.run(task=verify_bgp)
+  print_result(verify)
+  
   
 if __name__ == "__main__":
    main()
