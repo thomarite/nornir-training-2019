c149346215137b14ee2de8165b51e03bfb684665
tmorales <tomas.morales@gmail.com>
2019-10-23 16:14:40 -0700

    starting ex1 from week6

--- week06/ex1/ex1b.py
+++ week06/ex1/ex1b.py
@@ -5,7 +5,8 @@ from nornir.plugins.functions.text import print_result
 def send_command(task, cmd=""):
    results = task.run(task=netmiko_send_command, command_string=cmd)
    if "syntax error" in results[0].result:
-      print("ERROR DETECTED!")
+      #print("ERROR DETECTED!")
+      raise ValueError("Invalid Junos command")
 
 if __name__ == "__main__":
    nr = InitNornir(config_file="config.yaml")
